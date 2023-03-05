2a687d1359e943245da2d3d3b2ca25a6e083af82
Aaron Morgan <aaron.morgan.7@outlook.com>
2023-03-05 15:53:16 -0700

    fixed login form issue

--- client/src/components/ProjectList/index.js
+++ client/src/components/ProjectList/index.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const ProjectList = ({
-    projects,
-    projectTitle,
-    showTitle = true,
-    showUsername = true,
+	projects,
+	projectTitle,
+	showTitle = true,
+	showUsername = true,
 }) => {
-    if(!projects) {
-        return <h3>No Projects Yet</h3>
-    }
+	if (!projects) {
+		return <h3>No Projects Yet</h3>;
+	}
 
-    return (
-        <div className="col-12 col-md-10 mb-5">
-					<h3>Projects:</h3>
-					<ul>
-						{projects.map((project) => (
-							<li key={project._id}>{project.projectTitle}</li>
-						))}
-					</ul>
-				</div>
-    )
-}
+	return (
+		<div className="col-12 col-md-10 mb-5">
+			<h3>Projects:</h3>
+			<ul>
+				{projects.map((project) => (
+					<li key={project._id}>
+						<Link to={`/projects/${project._id}`}>
+							{showTitle && project.projectTitle}
+						</Link>
+						{showUsername && (
+							<span>
+								&nbsp; by {project.user ? project.user.username : "Unknown"}
+							</span>
+						)}
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+};
 
 export default ProjectList;
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,79 +1,70 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { useMutation } from "@apollo/client";
-import { LOGIN_USER } from "../utils/mutations";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 
-import Auth from "../utils/auth";
+import Auth from '../utils/auth';
 
 const Login = (props) => {
-	const [formState, setFormState] = useState({ email: "", password: "" });
+	const [email, setEmail] = useState('');
+	const [password, setPass] = useState('');
 	const [login, { error, data }] = useMutation(LOGIN_USER);
 	console.log(login);
 
 	const handleChange = (event) => {
 		const { name, value } = event.target;
-
-		setFormState({
-			...formState,
-			[name]: value,
-		});
+		
 	};
 
-	const handleFormSubmit = async (event) => {
-		event.preventDefault();
-		console.log(formState);
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		console.log(email, password);
 		try {
 			const { data } = await login({
-				variables: { ...formState },
+				variables: { email, password },
 			});
-
+			console.log(data);
 			Auth.login(data.login.token);
 		} catch (e) {
 			console.error(e);
 		}
-
-		// clear form values
-		setFormState({
-			email: "",
-			password: "",
-		});
 	};
 
 	return (
-		<div className="auth-form">
-			{" "}
+		<div className='auth-form'>
+			{' '}
 			{data ? (
 				<p>
-					Success! You may now head{" "}
-					<Link to="/Dashboard">back to the homepage.</Link>
+					Success! You may now head{' '}
+					<Link to='/Dashboard'>back to the homepage.</Link>
 				</p>
 			) : (
-				<form className="login-form" onSubmit={handleFormSubmit}>
-					<label htmlFor="email">Email</label>
+				<form className='login-form' onSubmit={handleSubmit}>
+					<label htmlFor='email'>Email</label>
 					<input
-						value={formState.email}
-						onChange={handleChange}
-						type="email"
-						placeholder="email.com"
-						id="email"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+						type='email'
+						placeholder='email.com'
+						id='email'
 					/>
-					<label htmlFor="password">Password</label>
+					<label htmlFor='password'>Password</label>
 					<input
-						value={formState.password}
-						onChange={handleChange}
-						type="password"
-						placeholder="******"
-						id="password"
+						value={password}
+						onChange={(e) => setPass(e.target.value)}
+						type='password'
+						placeholder='******'
+						id='password'
 					/>
-					<button className="clickBtn" type="submit">
+					<button className='clickBtn' type='submit'>
 						Log In
 					</button>
 				</form>
 			)}
-			<button className="linkBtn" onClick={() => props.onFormSwitch("signup")}>
+			<button className='linkBtn' onClick={() => props.onFormSwitch('signup')}>
 				Dont Have an account? Sign up here
 			</button>
-			{error && <div className="">{error.message}</div>}
+			{error && <div className=''>{error.message}</div>}
 		</div>
 	);
 };
