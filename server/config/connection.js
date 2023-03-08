13e9a36e2e9b6da4f50c14948fbcd15511b9dcca
prkrgreenwell <prkrgreenwell@gmail.com>
2023-03-07 21:56:57 -0700

    mongodb setup

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,10 @@
-const mongoose = require('mongoose');
+/** @format */
 
-mongoose.connect(
-	process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/kanban',
-	{
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	}
-);
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 module.exports = mongoose.connection;
