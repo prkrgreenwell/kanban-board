d00ec20d0dd9cb41b4dbc038e81c78ac61996c59
prkrgreenwell <prkrgreenwell@gmail.com>
2023-03-07 21:33:47 -0700

    saved more things

--- client/src/components/ToDoTaskList/index.js
+++ client/src/components/ToDoTaskList/index.js
@@ -2,19 +2,23 @@
 
 import React from "react";
 
-const ToDoTaskList = ({ tasks, description, columnId }) => {
+const ToDoTaskList = ({ tasks }) => {
   return (
     <div>
-      <div className="card text-bg-secondary">
-        <div className="card-body p-2">
-          <div className="card-title">TSK-154</div>
-          <p>This is a description of a item on the board.</p>
-          <div className="buttons">
-            <button className="btn btn-danger btn-sm">Move back</button>
-            <button className="btn btn-success btn-sm">Move up</button>
+      {tasks &&
+        tasks.map((task) => (
+          <div key={task._id} className="card text-bg-secondary">
+            <div className="card-body p-2">
+              <div className="card-title">{task.task}</div>
+              <p>{task.description}</p>
+              <div className="buttons">
+                <button className="btn btn-danger btn-sm">Move back</button>
+                <button className="btn btn-success btn-sm">Move up</button>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        ))}
+
       <div> &nbsp; </div>
     </div>
   );
