bd134d36633454ba0301995be68e33656cc7da14
Daniel Graves <68198944+danielcgraves@users.noreply.github.com>
2023-03-08 20:48:14 -0700

    dashboard styling

--- client/src/components/ProjectList/index.js
+++ client/src/components/ProjectList/index.js
@@ -1,29 +1,30 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ProjectList = ({
-  projects,
-  projectTitle,
-  showTitle = true,
-  showUsername = true,
+	projects,
+	projectTitle,
+	showTitle = true,
+	showUsername = true,
 }) => {
-  if (!projects) {
-    return <h3>No Projects Yet</h3>;
-  }
+	if (!projects) {
+		return <h3>No Projects Yet</h3>;
+	}
 
-  
-  return (
-    <div className="col-12 col-md-10 mb-5">
-      
-      <ul>
-        {projects && projects.map((project) => (
-          <li key={project._id}>
-            <Link className='projects' to={`/projects/${project._id}`} >{project.projectTitle}</Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+	return (
+		<div className='mb-4'>
+			<ul>
+				{projects &&
+					projects.map((project) => (
+						<li key={project._id}>
+							<Link className='projects' to={`/projects/${project._id}`}>
+								{project.projectTitle}
+							</Link>
+						</li>
+					))}
+			</ul>
+		</div>
+	);
 };
 
 export default ProjectList;
